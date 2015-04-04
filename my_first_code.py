506aa022aba0280eb4ac20d8c1fa9a5682b279eb
aaadia <privet.eto.olia@gmail.com>
2015-04-04 21:32:57 +0300

    files!
    files!

---
+++ my_first_code.py
@@ -0,0 +1,69 @@
+##hh = 'Hello, world!';
+##print (hh)
+
+##x = input ('input x: ')
+##y = input ('input y: ')
+##try:
+##    x = float (x)
+##    y = float (y)
+
+##if x > 0:
+##    print ('x is positive')
+##    pass
+##elif x==0:
+##    print ('x is ZERO!')
+##elif x==-1:
+##    print ('haaaa! it is the minus one!')
+##else: print ('x is negative')
+
+##    if y > x:
+##        print ('y is bigger than x: \n', y, '>', x)
+##    else:
+##        if x > y:
+##            print ('x is bigger than y: \n', x, '>', y)
+##        else: print ('x equals to y: \n', x, '=', y)
+##except:
+##    print ('Input numbers!')
+
+##import math
+##signal_power = 9
+##noise_power = 10
+##ratio = signal_power / noise_power
+##decibels = 10 * math.log10(ratio)
+##print (decibels)
+
+#-------------------
+
+import random, math
+
+##for i in range(10):
+##    x = random.random()
+##    print (x)
+
+a = math.pi / 4
+
+def tt ():
+    two_prints(a)
+    two_prints(a)
+
+def two_prints(a):
+    b = math.sin(a)
+    c = math.cos(a)
+    print (b)
+    print (c)
+    print ('haaa-haa!')
+    print ("double quotes")
+
+tt()
+
+#trretgrtgrtrtrtrtrtrtrrtr!!!!!!!!!!!!!!!!
+
+
+
+
+
+
+
+
+
+
---
+++ mycode.py
@@ -0,0 +1,74 @@
+# this is super program!
+
+# i = input ('input your money!\n'); #just input
+# print (i);
+# i = int(i);
+# h = i*10;
+# print ('real money:', h, ', yeah!\n');
+
+# ----- ex 2.2 ------
+
+## name = input ('Enter your name:\n');
+## print ('Hello,', name, '!\n')
+       
+
+# ----- ex 2.3 ------
+
+##hours = input ('Enter Hours: ');
+##rate = input ('Enter Rate: ');
+##hours = float (hours);
+##rate = float (hours);
+##pay = hours * rate;
+##print ('Pay:', pay)
+
+# ----- ex 2.4 Fahrenheit ------
+
+##tC = input ('Enter temetature in Celsius: '); 
+##tC = float (tC);
+##tF = (9/5) * tC + 32;
+##print ('The temerature in Fahrenheit is:', tF)
+
+# ----- ex 3.1, ex 3.2 -------
+
+##h = input ('Enter Hours: ')
+##try:
+##    h = float (h)
+##    r = input ('Enter Rate: ')
+##    try:       
+##        r = float (r)
+##        if h > 0 and r > 0:
+##            if h < 40:
+##                tr = h * r
+##            else:
+##                tr = (h - 40) * r * 1.5 + 40 * r
+##            print ('Total pay: ', tr)
+##        else: print ('Hours and rate must be positive!') 
+##    except:
+##        print ('ERROR! Enter numbers!')
+##except:
+##        print ('ERROR! Enter numbers!')
+        
+# ----- ex 3.3 -------
+
+a = input ('Input score: ')
+try:
+    a = float (a)
+    if a <= 1 and a >= 0:
+        if a < 0.6: print ('F')
+        elif a < 0.7: print ('D')
+        elif a < 0.8: print ('C')
+        elif a < 0.9: print ('B')
+        else: print ('A')
+    else: print ('Bad score')
+except:
+    print ('Bad score')
+    
+
+#one more comment!
+
+
+
+
+
+
+
